4d520434bd6e4f690d0467af4a700a86026b45a0
Jean Nunes <jean.nunes@uzzipay.com>
2025-04-22 15:00:48 -0400

    Add font

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
 import { Groups } from "@screens/Groups";
 import { ThemeProvider } from "styled-components";
 
+import { ActivityIndicator } from "react-native";
 import theme from "./src/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
