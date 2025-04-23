b89e304fa34beb1282d00fe47a7186b9aab4f853
Jean Nunes <jean.nunes@uzzipay.com>
2025-04-22 21:25:49 -0400

    Set status bar

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import { Groups } from "@screens/Groups";
 import { ThemeProvider } from "styled-components";
 
 import Loading from "@components/Loading";
+import { StatusBar } from "react-native";
 import theme from "./src/theme";
 
 export default function App() {
@@ -17,6 +18,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
