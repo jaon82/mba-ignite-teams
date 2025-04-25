e8c01466c21ea09afb661aeba46c0f56114b4115
Jean Nunes <jean.nunes@uzzipay.com>
2025-04-24 23:46:54 -0400

    Update Players screen

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,17 +1,25 @@
 import Header from "@components/Header";
 import { Highlight } from "@components/Highlight";
 
-import { Container } from "./styles";
+import { ButtonIcon } from "@components/ButtonIcon";
+import { Input } from "@components/Input";
+import { Container, Form } from "./styles";
 
 export function Players() {
-    return (
-        <Container>
-            <Header showBackButton />
+  return (
+    <Container>
+      <Header showBackButton />
 
-            <Highlight
-                title="Nome da turma"
-                subtitle="adicione a galera e separe os times"
-            />
-        </Container>
-    );
+      <Highlight
+        title="Nome da turma"
+        subtitle="adicione a galera e separe os times"
+      />
+
+      <Form>
+        <Input placeholder="Nome da pessoa" autoCorrect={false} />
+
+        <ButtonIcon icon="add" />
+      </Form>
+    </Container>
+  );
 }
