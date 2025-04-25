39a4d25a9efe56812b7719701f2452d93dd0f3d6
Jean Nunes <jean.nunes@uzzipay.com>
2025-04-25 00:02:07 -0400

    Add Filter

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -2,10 +2,16 @@ import Header from "@components/Header";
 import { Highlight } from "@components/Highlight";
 
 import { ButtonIcon } from "@components/ButtonIcon";
+import { Filter } from "@components/Filter";
 import { Input } from "@components/Input";
-import { Container, Form } from "./styles";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 export function Players() {
+  const [team, setTeam] = useState("Time A");
+  const [players, setPlayers] = useState([]);
+
   return (
     <Container>
       <Header showBackButton />
@@ -20,6 +26,23 @@ export function Players() {
 
         <ButtonIcon icon="add" />
       </Form>
+
+      <HeaderList>
+        <FlatList
+          data={["Time A", "Time B"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+      </HeaderList>
     </Container>
   );
 }
