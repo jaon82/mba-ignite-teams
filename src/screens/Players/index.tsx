f0892901b025a6a5da5be3aa608528f476100e20
Jean Nunes <jean.nunes@uzzipay.com>
2025-04-25 00:10:52 -0400

    Finish Players screen

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,9 +1,11 @@
 import Header from "@components/Header";
 import { Highlight } from "@components/Highlight";
 
+import { Button } from "@components/Button";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Filter } from "@components/Filter";
 import { Input } from "@components/Input";
+import { ListEmpty } from "@components/ListEmpty";
 import { PlayerCard } from "@components/PlayerCard";
 import { useState } from "react";
 import { FlatList } from "react-native";
@@ -11,7 +13,7 @@ import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 export function Players() {
   const [team, setTeam] = useState("Time A");
-  const [players, setPlayers] = useState(["Rodrigo", "Vini"]);
+  const [players, setPlayers] = useState([]);
 
   return (
     <Container>
@@ -51,7 +53,17 @@ export function Players() {
         renderItem={({ item }) => (
           <PlayerCard name={item} onRemove={() => {}} />
         )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time" />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 },
+        ]}
       />
+
+      <Button title="Remover Turma" type="SECONDARY" />
     </Container>
   );
 }
