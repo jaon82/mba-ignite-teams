2a88e4df51875c5c379e3827583c42ff7938d4c4
Jean Nunes <jean.nunes@uzzipay.com>
2025-04-24 22:29:14 -0400

    Add GroupCard component

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,5 +2,6 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  background-color: ${(props) => props.theme.COLORS.GRAY_600};
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+  padding: 24px;
 `;
