8f402b3c8eab6be0afd943886eb32e3488ca68b8
Jean Nunes <jean.nunes@uzzipay.com>
2025-04-26 00:27:33 -0400

    Add go back action

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { BackButton, BackIcon, Container, Logo } from "./styles";
 
 import logoImg from "@assets/logo.png";
@@ -5,10 +6,16 @@ import logoImg from "@assets/logo.png";
 type Props = { showBackButton?: boolean };
 
 export default function Header({ showBackButton = false }: Props) {
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.navigate("groups");
+  }
+
   return (
     <Container>
       {showBackButton && (
-        <BackButton>
+        <BackButton onPress={handleGoBack}>
           <BackIcon />
         </BackButton>
       )}
